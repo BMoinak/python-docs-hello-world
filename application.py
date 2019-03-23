3610338f2836d96563a30f2c13b534a35d2601f9
Moinak Bandyopadhyay <moinakbanerjee.98@gmail.com>
2019-03-24 02:37:35 +0530

    Update application.py
    using pickle

--- application.py
+++ application.py
@@ -29,20 +29,20 @@ def knnmodel():
     knn_recall_score=recall_score(test_labels,knn_predicted_test_labels)
     knn_f1_score=f1_score(test_labels,knn_predicted_test_labels)
     return knn_accuracy_score
-# def knnmodtrain():
-#     with open("knn.pkl", "rb") as f:
-#         knn = pickle.load(f)
-#     data = pd.read_csv("test.csv")
-#     X = data.drop("fraud", axis = 1)
-#     y = data["fraud"]
-#     y_knn = knn.predict(X)
-#     y_num = y.values
-#     return accuracy_score(y_num,y_knn)
+def knnmodtrain():
+    with open("knn.pkl", "rb") as f:
+        knn = pickle.load(f)
+    data = pd.read_csv("test.csv")
+    X = data.drop("fraud", axis = 1)
+    y = data["fraud"]
+    y_knn = knn.predict(X)
+    y_num = y.values
+    return accuracy_score(y_num,y_knn)
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    x = knnmodel()
-    y="Accuracy of the model is: " + str(x) + pickle.__doc__
+    x = knnmodtrain()
+    y="Accuracy of the model is: " + str(x)
     return y
